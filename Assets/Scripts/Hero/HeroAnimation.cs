ad3f787351a419c8607862811c59c4e8899c7498
Евгений <listopadov_94@mail.ru>
2024-09-11 15:21:17 +0300

    анимация движения героя

---
+++ Assets/Scripts/Hero/HeroAnimation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Scripts.Hero
+{
+    public class HeroAnimation : MonoBehaviour
+    {
+        private const string IsMove = "IsMove";
+
+        [SerializeField] private Animator _animator;
+        [SerializeField] private CharacterController _characterController;
+
+        private void Update()
+        {
+            if (_characterController.velocity.sqrMagnitude > 0.05f)
+                _animator.SetBool(IsMove, true);
+            else
+                _animator.SetBool(IsMove, false);
+        }
+    }
+}
